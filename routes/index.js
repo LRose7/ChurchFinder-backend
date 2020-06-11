403fdfe0f94d0b50eca05d724a783cb54e2f0d61
ElleRose <lindseyurquhart@tutanota.com>
2020-06-11 17:47:56 -0400

    connected to database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,25 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+const dotenv = require('dotenv').config();
+
+console.log(dotenv.parsed);
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log('Yay! You are connected to the database!');
+})
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
