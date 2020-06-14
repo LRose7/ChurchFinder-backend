be906da32e79f75b22ce3c505f266f5511177cf8
ElleRose <lindseyurquhart@tutanota.com>
2020-06-13 20:43:10 -0400

    added columns and datatypes to churches table

--- models/churches.js
+++ models/churches.js
@@ -1,15 +1,65 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Churches = sequelize.define('Churches', {
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    mailing_one: DataTypes.STRING,
-    mailing_two: DataTypes.STRING,
-    city: DataTypes.STRING,
-    state: DataTypes.STRING
-  }, {});
-  Churches.associate = function(models) {
-    // associations can be defined here
-  };
-  return Churches;
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('churches', {
+    Id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    Name: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    Description: {
+      type: "BLOB",
+      allowNull: true
+    },
+    Mailing_One: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    Mailing_Two: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    City: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    State: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    PostalCode: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    Denomination: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    Web_URL: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    Latitude: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    Longitude: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+    tableName: 'churches'
+  });
 };
