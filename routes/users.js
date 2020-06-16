cf2d48c6c2695de4943ff1316dc3e4b9ed78412f
ElleRose <lindseyurquhart@tutanota.com>
2020-06-16 17:52:40 -0400

    signup working

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
-const axios = require('axios').default;
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-axios.post('/signup', function(req, res, next) {
+router.post('/signup', function(req, res, next) {
   models.users
     .findOrCreate({
       where: {
